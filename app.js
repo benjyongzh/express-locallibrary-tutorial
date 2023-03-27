970fdb9c2d19aab874e5a5df8bbfb5ed5e7495f5
Benjamin Yong <benj_yongzh@hotmail.com>
2023-03-27 23:43:47 +0800

    Add dotenv and db environment variable.

--- app.js
+++ app.js
@@ -12,8 +12,11 @@ var app = express();
 //set up mongoose connection
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
-const mongoDB =
-  "mongodb+srv://admin:MNBNVCXz1!@cluster0.lvdficv.mongodb.net/local_library?retryWrites=true&w=majority";
+
+const { config } = require("dotenv");
+config();
+
+const mongoDB = process.env.DATABASE;
 
 mainModule().catch((err) => console.log(err));
 async function main() {
