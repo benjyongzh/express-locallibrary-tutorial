faeecb38e932f9d5d42446937f0ad75015d8d5b4
Benjamin Yong <benj_yongzh@hotmail.com>
2023-04-11 23:34:11 +0800

    added author deleting function. tested.

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -119,13 +119,75 @@ exports.author_create_post = [
 ];
 
 //display delete form for author, on GET
-exports.author_delete_get = (req, res) => {
-  res.send("NOT IMPLEMENTED: Author delete GET");
+exports.author_delete_get = (req, res, next) => {
+  async.parallel(
+    {
+      author(callback) {
+        Author.findById(req.params.id).exec(callback);
+      },
+      authors_books(callback) {
+        Book.find({ author: req.params.id }).exec(callback);
+      },
+    },
+    (err, results) => {
+      if (err) {
+        return next(err);
+      }
+
+      if (results.author == null) {
+        //no results
+        res.redirect("/catalog/authors");
+      }
+
+      //success
+      res.render("author_delete", {
+        title: "Delete Author",
+        author: results.author,
+        author_books: results.authors_books,
+      });
+    }
+  );
 };
 
 //handle for author delete POST form
-exports.author_delete_post = (req, res) => {
-  res.send("NOT IMPLEMENTED: Author delete POST");
+exports.author_delete_post = (req, res, next) => {
+  async.parallel(
+    {
+      author(callback) {
+        Author.findById(req.body.authorid).exec(callback);
+      },
+      authors_books(callback) {
+        Book.find({ author: req.body.authorid }).exec(callback);
+      },
+    },
+
+    (err, results) => {
+      if (err) {
+        return next(err);
+      }
+
+      //success
+      if (results.authors_books.length > 0) {
+        //author has books. render in same way as GET route
+        res.render("author_delete", {
+          title: "Delete Author",
+          author: results.author,
+          author_books: results.authors_books,
+        });
+        return;
+      }
+
+      //author has no books. Delete object and redirect to the list of authors
+      Author.findByIdAndRemove(req.body.authorid, (err) => {
+        if (err) {
+          return next(err);
+        }
+
+        //success - go to author list
+        res.redirect("/catalog/authors");
+      });
+    }
+  );
 };
 
 //display update form for author, on GET
